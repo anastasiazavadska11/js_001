f05aafff1b8f150e6080092435dc20296f2a6472
anastasiazavadska11 <zavadska.anastasiia@gmail.com>
2024-03-28 20:28:13 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,31 @@
+const age = 12;
+console.log(age);
+
+const name = "Anastasiia";
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "Якщо воно дебіл, то йому потрібно про це сказати, бо воно ж не в курсі.";
+console.log(myString);
+
+let myNumber = 21;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+let myNull = null;
+console.log(myNull);
+
+const hello = prompt("What is your name?");
+alert("Welcome " + hello + "!");
+
+const test = confirm("Are you a dog?");
+if (test) alert("Дякую за підтвердження!");
+else alert("Дію відмінено!");
+
+const danger = alert("Увага! Повітряна тривога!!!");
+
+const dangerConfirm = confirm("Ви впевнені, що вам треба на вулицю?");
+if (dangerConfirm) alert("Дякую за підтвердження!");
+else alert("Дію відмінено!");
